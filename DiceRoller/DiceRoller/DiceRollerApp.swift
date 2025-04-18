9bf016c71bea7f5693b917c8ab0107644da14d5d
Rifqi Rahman <rifqi.efforts@gmail.com>
2025-04-18 09:15:38 +0700

    Add files via upload

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Rifqi Rahman on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
